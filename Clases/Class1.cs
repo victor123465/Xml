3dced8e9d5a9b817ba042df7d69b48578d11cf4e
Victor <vicsal94@gmail.com>
2020-05-25 16:06:17 +0200

    Initial commit

---
+++ Clases/Class1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using static System.Console;
+using System.Threading;
+
+
+namespace Clases
+{
+    //Interface que se va a usar para los nodos de configuracion
+    interface IinfoNodo
+    {
+        void CrearNodo();
+        void ModificarNodo();
+    }
+   
+    //En esta clase se establece la fecha en el archivo de configuración
+    public class NodoFecha:IinfoNodo
+    {
+        public XmlDocument document=new XmlDocument();
+        public string NombreNodo;
+        public DateTime Fechas;
+        public NodoFecha(string nombre,DateTime fecha)//Constructor
+        {
+            this.NombreNodo=nombre;
+            this.Fechas=fecha;
+        }
+        public void CrearNodo()//Añade el nodo fecha al documento xml
+        {
+            document.Load("Configuracion.xml");
+            XmlNode root=document.DocumentElement;
+            XmlNode nodo=document.CreateElement(this.NombreNodo);
+            nodo.InnerText=this.Fechas.ToString("dd/MM/yyyy");
+            root.AppendChild(nodo);
+            document.Save("Configuracion.xml");
+        }
+        public void ModificarNodo()//Modifica el nodo por pantalla
+        {
+            string entrada=null;
+            WriteLine($"{this.NombreNodo}: {this.Fechas.ToString("dd/MM/yyyy")}");
+            while(entrada!="s" && entrada!="n")
+            {
+                Write($"¿Modificar {this.NombreNodo} ?\tSí-->s\tNo-->n");
+                Casilla casilla=new Casilla();
+                entrada=ReadLine();
+                WriteLine();
+            }
+            if(entrada=="s")
+            {
+                re:
+                Write("Fecha: ");
+                try{
+                    this.Fechas=DateTime.Parse(ReadLine());
+                }catch
+                {
+                    WriteLine("Fecha incorrecta.");
+                    goto re;
+                }
+                
+                document.Load("Configuracion.xml");
+                XmlNode root=document.DocumentElement;
+                XmlNode nod=root.SelectSingleNode(this.NombreNodo);
+                nod.InnerText=this.Fechas.ToString("dd/MM/yyyy");
+                document.Save("Configuracion.xml");
+            }
+            WriteLine("\n");
+        }
+        
+    }
+    //Nodo que establece tanto el color de fondo como el color del texto
+    public class NodoColor:IinfoNodo
+    {
+        public string NombreNodo;
+        public ConsoleColor Color;
+        public XmlDocument document=new XmlDocument();
+        public NodoColor(string nombre,ConsoleColor color)
+        {
+            this.NombreNodo=nombre;
+            this.Color=color;
+        }
+        public void CrearNodo()
+        {
+            document.Load("Configuracion.xml");
+            XmlNode root=document.DocumentElement;
+            XmlNode nodo=document.CreateElement(this.NombreNodo);
+            nodo.InnerText=this.Color.ToString();
+            root.AppendChild(nodo);
+            document.Save("Configuracion.xml");
+        }
+        public void ModificarNodo()
+        {
+            string entrada=null;
+            WriteLine($"{this.NombreNodo}: {this.Color}");
+            while(entrada!="s" && entrada!="n")
+            {
+                Write($"¿Modificar {this.NombreNodo} ?\tSí-->s\tNo-->n");
+                Casilla casilla=new Casilla();
+                entrada=ReadLine();
+                WriteLine();
+            }
+            if(entrada=="s")
+            {
+                document.Load("Configuracion.xml");
+                XmlNode root=document.DocumentElement;
+                XmlNode nodo=root.SelectSingleNode(this.NombreNodo);
+                ConsoleColor[] colors = (ConsoleColor[]) ConsoleColor.GetValues(typeof(ConsoleColor));
+
+                WriteLine("Color:");
+                int i=0;
+                int num=-1;
+                foreach(var color in colors)
+                {
+                    CursorLeft=25;
+                    WriteLine($"{color}-->{i++}");
+                }
+                while(num<0 || num>15)
+                {
+                    re:
+                    Write("Nuevo color: ");
+                    try{
+                        num=int.Parse(ReadLine());
+                    }catch
+                    {
+                        goto re;
+                    }
+                }
+                ConsoleColor color1=colors[num];
+                nodo.InnerText=$"{color1}";
+                document.Save("Configuracion.xml");
+            }
+            WriteLine("\n");
+        }
+        
+    }
+    //Nodo que establece las dimensiones de la ventana
+    public class NodoDimension:IinfoNodo
+    {
+        public string NombreNodo;
+        public int Dimension;
+        public XmlDocument document=new XmlDocument();
+        public NodoDimension(string nombre,int dimension)
+        {
+            this.NombreNodo=nombre;
+            this.Dimension=dimension;
+        }
+        public void CrearNodo()
+        {
+            document.Load("Configuracion.xml");
+            XmlNode root=document.DocumentElement;
+            XmlNode nodo=document.CreateElement(this.NombreNodo);
+            nodo.InnerText=this.Dimension.ToString();
+            root.AppendChild(nodo);
+            document.Save("Configuracion.xml");
+        }
+        public void ModificarNodo()
+        {
+            string entrada=null;
+            WriteLine($"{this.NombreNodo}: {this.Dimension}");
+            while(entrada!="s" && entrada!="n")
+            {
+                Write($"¿Modificar {this.NombreNodo} ?\tSí-->s\tNo-->n");
+                Casilla casilla=new Casilla();
+                entrada=ReadLine();
+                WriteLine();
+            }
+            if(entrada=="s")
+            {
+                document.Load("Configuracion.xml");
+                XmlNode root=document.DocumentElement;
+                XmlNode nodo=root.SelectSingleNode(this.NombreNodo);
+                int alto=0;
+                re:
+                Write($"{this.NombreNodo}: ");
+                try{
+                    alto=int.Parse(ReadLine());
+                }catch
+                {
+                    goto re;
+                }
+                nodo.InnerText=$"{alto}";
+                document.Save("Configuracion.xml");
+            }
+            WriteLine("\n");
+        }
+    }
+    //En esta clase se obtiene el espacio libre en el disco duro
+    public class NodoEspacioLibre:IinfoNodo
+    {
+        public string NombreNodo;
+        public XmlDocument document=new XmlDocument();
+        public NodoEspacioLibre(string nombre)
+        {
+            this.NombreNodo=nombre;
+        }
+        public void CrearNodo()
+        {
+            document.Load("Configuracion.xml");
+            XmlNode root=document.DocumentElement;
+            DriveInfo[] allDrivers=DriveInfo.GetDrives();
+            foreach(DriveInfo d in allDrivers)
+            {
+                if(d.Name=="C:\\")
+                {
+                    XmlNode esplibre=document.CreateElement("EspacioLibre");
+                    esplibre.InnerText=$"{d.AvailableFreeSpace}";
+                    root.AppendChild(esplibre);
+                }
+            }
+            document.Save("Configuracion.xml");
+        }
+        public void ModificarNodo()
+        {     
+        }
+    }
+    public class Usuario//Clase para generar los usuarios con nombre, contraseña y tipo de usuario
+    {
+       public string Nombre;
+        public string Contraseña;
+        public string Tipo;
+        public Usuario(string nombre,string contraseña,int tipo)
+        {
+            this.Nombre=nombre;
+            this.Contraseña=contraseña;
+            if(tipo==1)
+            {
+                this.Tipo="Administrador";
+            }else if(tipo==2)
+            {
+                this.Tipo="Limitado";
+            }
+        }
+        
+    }
+    //En la clase AñadirUsuario se hereda las propiedades de Usuario y se añaden al documento
+    public class AñadirUsuario:Usuario
+    {
+        public AñadirUsuario(string nombre,string contraseña,int tipo):base(nombre,contraseña,tipo)
+        {
+            XmlDocument doc =new XmlDocument(); //Nuevo documento xml
+
+            doc.Load("Configuracion.xml");    //Cargo el documento
+            XmlNode root=doc.DocumentElement; //Cargo el nodo principal Configuracion
+            XmlNode node=doc.CreateElement("Usuario");   //Genero el nodo correspondiente al usuario
+
+            XmlNode tipox=doc.CreateElement("Tipo");
+            tipox.InnerText=this.Tipo;
+
+            XmlNode nombrex=doc.CreateElement("Nombre"); //Creo el nodo Nombre
+            nombrex.InnerText=this.Nombre; //Establezco el nombre del usuario
+
+            XmlNode contraseñax=doc.CreateElement("Contraseña");//Creo el nodo contraseña
+            contraseñax.InnerText=this.Contraseña; //Establezco la contraseña
+
+            node.AppendChild(tipox);
+            node.AppendChild(nombrex); //Añado el nombre al tipo de usuario
+            node.AppendChild(contraseñax); //Añado la contraseña al tipo de usuario
+            root.AppendChild(node);//Añado el tipo de usuario a la configuracion
+
+            doc.Save("Configuracion.xml");  //Guardo el archivo Configuracion.xml
+        }
+    }
+     public class Leer //Clase para leer el documento xml y mostrarlo por consola
+    {
+        public Leer()
+        {
+            XmlDocument document=new XmlDocument();
+            document.Load("Configuracion.xml");
+            XmlNode root=document.DocumentElement;
+            XmlNodeList lista=root.ChildNodes;
+
+            foreach(XmlNode nodo in lista)
+            {
+                ForegroundColor=ConsoleColor.Blue;
+                WriteLine($"<{nodo.Name}>");
+                foreach(XmlNode nod in nodo.ChildNodes)
+                {
+                    ForegroundColor=ConsoleColor.Green;
+                    Write($"\t<{nod.Name}>");
+                    ForegroundColor=ConsoleColor.White;
+                    Write($"{nod.InnerText}");
+                    ForegroundColor=ConsoleColor.Green;
+                    WriteLine($"</{nod.Name}>");
+                }
+                ForegroundColor=ConsoleColor.Blue;
+                WriteLine($"</{nodo.Name}>");
+            }
+            ForegroundColor=ConsoleColor.Gray;
+        }
+    }
+    public class Casilla//Clase que establece una casilla donde el usuario responde por pantalla.
+    {
+        public Casilla()
+        {
+            SetCursorPosition(44,CursorTop-1);
+            Write("┌");
+            SetCursorPosition(48,CursorTop);
+            Write("┐");
+            SetCursorPosition(44,CursorTop+2);
+            Write("└");
+            SetCursorPosition(48,CursorTop);
+            Write("┘");
+            SetCursorPosition(46,CursorTop-1);
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Clases;
+using static System.Console;
+
+namespace Xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cabecera("Xml");
+
+            //Se establecen los datos de configuración iniciales.
+            DateTime date=DateTime.Now;
+            ConsoleColor colorfondo=ConsoleColor.Cyan;
+            ConsoleColor colortexto=ConsoleColor.Yellow;
+            int alto=30;
+            int ancho=90;
+
+     
+
+            //Se generan las clases para cada atributo de la configuración
+            NodoFecha Fecha=new NodoFecha("Fecha",date);
+            NodoColor ColorFondo=new NodoColor("ColorFondo",colorfondo);
+            NodoColor ColorTexto=new NodoColor("ColorTexto",colortexto);
+            NodoDimension Alto=new NodoDimension("Alto",alto);
+            NodoDimension Ancho=new NodoDimension("Ancho",ancho);
+            NodoEspacioLibre EspacioLibre=new NodoEspacioLibre("EspacioLibre");
+
+            //Compruebo si existe el documento Configuracion.xml y si no existe lo genera y añade los datos de configuracion iniciales
+            if(!(File.Exists("Configuracion.xml")))
+            {
+                XmlDocument document=new XmlDocument();
+                XmlDeclaration declaration=document.CreateXmlDeclaration("1.0","UTF-8",null);
+                document.AppendChild(declaration);
+                XmlNode root=document.CreateElement("Configuracion");
+                document.AppendChild(root);
+                document.Save("Configuracion.xml");
+
+                Fecha.CrearNodo();
+                ColorFondo.CrearNodo();
+                ColorTexto.CrearNodo();
+                Alto.CrearNodo();
+                Ancho.CrearNodo();
+                EspacioLibre.CrearNodo();
+
+                WriteLine("Se ha generado el documento \"Configuracion.xml\"");
+
+            }
+            WriteLine("Ya existe el archivo \"Configuracion.xml\"\n");
+            //A continuación pregunta si quiere modificar los datos por pantalla.
+            WriteLine("Modifique si quiere los elementos de la configuración");
+            Fecha.ModificarNodo();
+            ColorFondo.ModificarNodo();
+            ColorTexto.ModificarNodo();
+            Alto.ModificarNodo();
+            Ancho.ModificarNodo();
+
+            //Pido que se introduzcan por pantalla los usuarios
+            int orden=0;
+            int tipo=0;
+            while(orden!=1 && orden!=2)
+            { 
+                Nuevo:
+                Write("Añadir usuario --> 1 \t Salir --> 2\t");
+                Casilla();
+                if(int.TryParse(ReadLine(),out int num))
+                {
+                    orden=num;
+                }
+                WriteLine("\n");
+
+                //Procedimiento para añadir usuarios o salir del programa
+                switch(orden)
+                {
+                    case 1:
+                        Write($"Nombre: ");
+                        string nombre=ReadLine();
+                        Write("Contraseña: ");
+                        string contraseña=ReadLine();
+                        while(tipo!=1 && tipo!=2)
+                        {
+                            Write("Administrador --> 1 \t Limitado --> 2 \t");
+                            Casilla();
+                            if(int.TryParse(ReadLine(),out num))
+                            {
+                                tipo=num;
+                            }
+                            WriteLine();
+                        }
+                        AñadirUsuario usuarionuevo=new AñadirUsuario(nombre,contraseña,tipo);
+                        WriteLine("\n");
+                        goto Nuevo;
+                    case 2:
+                        break;
+                    default:
+                        goto Nuevo;
+                }
+
+                WriteLine("El documento \"Configuracion.xml\" contiene:");
+                Leer xml=new Leer();
+            }
+        }
+        public static void Cabecera(string nombreprograma)//Esta es la cabecera.
+        {
+            //Dimensiones de la ventana
+            int ancho=90;
+            int alto=30;
+            SetWindowSize(ancho,alto);
+
+            //Limpio lo que haya antes;
+            Clear();
+            
+            //Dibujo los bordes horizontales
+            for(int i=1;i<ancho-1;i++)
+            {
+                
+                
+                Write("═");
+                SetCursorPosition(i,4);
+                Write("═");
+                SetCursorPosition(i,0);
+                Write("═");
+            }
+            //Dibujo las esquinas
+            SetCursorPosition(0,4);
+            WriteLine("╚");
+            SetCursorPosition(ancho-1,0);
+            WriteLine("╗");
+            SetCursorPosition(0,0);
+            WriteLine("╔");
+            SetCursorPosition(ancho-1,4);
+            Write("╝");
+            
+            //Dibujo los bordes verticales
+            for(int i=1;i<4;i++)
+            {
+                SetCursorPosition(ancho-1,i);
+                
+                Write("║");
+                SetCursorPosition(0,i);
+                Write("║");
+            }
+            
+            //Escribo los datos de título del programa, autor, fecha y hora
+            ForegroundColor=ConsoleColor.White;
+            //Fecha
+            SetCursorPosition(1,1);
+            DateTime fecha=DateTime.Now;
+            WriteLine($"Fecha:{fecha.ToString("dd/MM/yyyy")}");
+
+            //Nombre del programa
+            SetCursorPosition(1,2);
+            WriteLine($"Nombre del programa: {nombreprograma}");
+
+            //Nombre del autor del programa
+            SetCursorPosition(1,3);
+            Write("Autor: Víctor Salas Moreno");
+
+            //Hora
+            SetCursorPosition(ancho-15,1);
+            Write($"Hora: {fecha.ToString("HH:mm:ss")}");
+
+            //Situo el cursor fuera de la caja.
+            SetCursorPosition(0,6);
+            
+            
+            ResetColor();
+        }
+        public static void Casilla()//Marcador donde el usuario responderá por pantalla.
+        {
+            SetCursorPosition(44,CursorTop-1);
+            Write("┌");
+            SetCursorPosition(48,CursorTop);
+            Write("┐");
+            SetCursorPosition(44,CursorTop+2);
+            Write("└");
+            SetCursorPosition(48,CursorTop);
+            Write("┘");
+            SetCursorPosition(46,CursorTop-1);
+        }
+    }
+    
+}
